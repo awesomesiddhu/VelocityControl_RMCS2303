bad92e433c751817d3bc48f3f1edf3d604db0b93
awesomesiddhu <106588411+awesomesiddhu@users.noreply.github.com>
2023-10-30 11:16:39 +0530

    Update motor_controller1.py

--- motor_controller1.py
+++ motor_controller1.py
@@ -1,44 +1,67 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 import minimalmodbus
-import serial
 import math
 
-class MotorController(Node):
-    def __init__(self):
-        super().__init__('motor_controller')
+class MinimalModbusNode(Node):
+	def __init__(self):
+		super().__init__('minimal_modbus_node')
+		self.subscription = self.create_subscription(
+		Twist,
+		'cmd_vel',
+		self.listener_callback,
+		10)
+		self.subscription  # prevent unused variable warning
 
-        self.right_motor = minimalmodbus.Instrument('/dev/serial1', 1, minimalmodbus.MODE_ASCII)  # port name, slave address (in decimal), mode
+		self.rmcs2303_right = minimalmodbus.Instrument('/dev/ttyUSB0', 7, minimalmodbus.MODE_ASCII)
+		self.rmcs2303_left = minimalmodbus.Instrument('/dev/ttyUSB0', 6, minimalmodbus.MODE_ASCII)
 
-        self.right_motor.serial.baudrate = 9600
+		self.rmcs2303_right.serial.baudrate = 9600
+		self.rmcs2303_left.serial.baudrate = 9600
 
-        self.wheel_radius = 0.1 #meters
-        self.gear_ratio = 90
+	def listener_callback(self, msg):
+		gear_ratio = 90
+		wheel_radius = 0.1 #meters
+		lin_velocity = msg.linear.x # m/s
+		ang_velocity = msg.angular.z
+		velocity = lin_velocity + 0.05*ang_velocity
+		rpm= abs((velocity / (2*math.pi*wheel_radius))*60*gear_ratio)
 
-        velocity_mps = 1.0 #m/s
+		try:
+			self.rmcs2303_right.write_register(2, 2048, number_of_decimals=0, functioncode=6, signed=False) # set encoder count to 0
+			self.rmcs2303_right.write_register(14, rpm, number_of_decimals=0, functioncode=6, signed=False)  # Speed command for base motor in rpm
 
-        rpm = (velocity_mps / (2 * math.pi * self.wheel_radius)) * 60 * self.gear_ratio
+			if (velocity>=0):
+				self.rmcs2303_right.write_register(2, 257, number_of_decimals=0, functioncode=6, signed=False) # Enable motor in CW
+			else:
+				self.rmcs2303_right.write_register(2, 265, number_of_decimals=0, functioncode=6, signed=False) # Enable motor in CCW
 
-        # Ensure base_motor_rpm is within the valid range (0-65535)
-        rpm = min(max(int(rpm), 0), 65535)
 
-        if velocity_mps >= 0:
-            # Write base motor RPM to appropriate register
-            self.right_motor.write_register(2048, rpm, functioncode=6)
+			right_speed_feedback = self.rmcs2303_right.read_register(24) #current speed feedback
+			#print("Speed feedback : ", right_speed_feedback)
+			print("Velocity : ", velocity)
 
-            self.right_motor.write_register(257, 1, functioncode=6)
-        else:
-            # Write base motor RPM to appropriate register
-            self.right_motor.write_register(2048, -rpm, functioncode=6)
+		except KeyboardInterrupt:
+			if (velocity>=0):
+				self.rmcs2303_right.write_register(2, 256, number_of_decimals=0, functioncode=6, signed=False) # disable motor in CW
+			else:
+				self.rmcs2303_right.write_register(2, 264, number_of_decimals=0, functioncode=6, signed=False) # disable motor in CCW
 
-            self.right_motor.write_register(265, 1, functioncode=6)
+			self.rmcs2303_right.write_register(2, 2304, number_of_decimals=0, functioncode=6, signed=False) #restarts the drive
+			self.rmcs2303_right.write_register(2, 2304, number_of_decimals=0, functioncode=6, signed=False) #restarts the drive
+
 def main(args=None):
-    rclpy.init(args=args)
-    motor_controller = MotorController()
-    rclpy.spin(motor_controller)
+	rclpy.init(args=args)
+
+	minimal_modbus_node = MinimalModbusNode()
+
+	rclpy.spin(minimal_modbus_node)
+
+	minimal_modbus_node.destroy_node()
+	rclpy.shutdown()
 
 if __name__ == '__main__':
-    main()
+	main()
